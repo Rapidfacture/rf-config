d2647a8792dd2588bbf8aaabb461c0107e962ed2
jvm <git@jinvent.de>
2017-08-24 10:32:20 +0200

    Added default config example

---
+++ config/conf.default/config.js
@@ -0,0 +1,10 @@
+/* Configuration */
+
+module.exports = {
+   "abc": "def",
+   "paths": {
+      "myReadme" : paths.root + "/README.md",
+      "myTxtConfig" : paths.config + "/otherconfigfile.txt",
+   },
+};
+
