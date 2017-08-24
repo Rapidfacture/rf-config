9d8bdd6aa6e51ccea252693ec6d5f0308d094e02
jvm <git@jinvent.de>
2017-08-24 10:28:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# rf-config
+
+* Simple NodeJS config read-in lib
+* Optional dependency: Uses rf-log for error logging if available
+* Configurable base dir
+* Creates a config variable with object from ./config/conf/config.js, if directory is available. As a fallback, ./config/conf.default/ is used.
+* Additionally, contents from ./package.json are inserted into that same configuration variable
+* The existence of all paths you define in your config.js in the "paths" key is verified and errors are logged
+
+## Getting Started
+
+> npm install rf-config
+
+config.js files are of this format:
+```js
+module.exports = {
+   "abc": "def",
+   "paths": {
+      "myReadme" : paths.root + "/README.md",
+      "myTxtConfig" : paths.config + "/otherconfigfile.txt",
+   },
+};
+```
+
+To load the configuration, you use a single line:
+```js
+config = require("rf-config").loadFrom(__dirname);
+
+console.log(config.paths.root); // our root path "__dirname"
+console.log(config.paths.config); // configuration directory
+console.log(config.paths.myReadme);
+console.log(config.packageJson); // our package.json data
+```
+
+## Legal Issues
+* Licenese: MIT
+* Author: Felix Furtmayr, Julian von Mendel
+
---
+++ index.js
@@ -0,0 +1,57 @@
+// rf-config, a small config loading lib for NodeJS
+
+var fs = require("fs"); // Filesystem operations
+var logError = console.log;
+
+// use rf-log for error logging if available
+try {
+   logError = require(require.resolve("rf-log")).error;
+} catch(e) {}
+
+module.exports.loadFrom = function (dirname) {
+   // Determine configuration directory
+   var configPath;
+   checkPath(dirname + "/config/conf.default"); // set path to standard path
+   checkPath(dirname + "/config/conf"); // custom folder? => set path to it
+
+   function checkPath(path){
+      if (fs.existsSync(path)){
+         configPath = path;
+      }
+   }
+
+   // Add paths to configuration
+   paths = {
+      root : dirname,
+      config : configPath,
+   };
+
+   var config = require(configPath + "/config.js"); // import global configuration
+   config.paths = ((typeof config.paths == 'object') ? config.paths : {});
+   Object.keys(paths).forEach(function (key) {
+      config.paths[key] = paths[key];
+   });
+
+   // Verify existence of all config paths
+   Object.keys(config.paths).forEach(function (key) {
+      var path = config.paths[key];
+      if (!fs.existsSync(path)) {
+         logError("Directory '" + path + "' does not exist -- please create it.");
+         return;
+      }
+   });
+   
+   // Add package.json to configuration
+   var packageJson = fs.readFileSync(dirname + '/package.json',
+         { encoding: 'utf8' });
+   
+   config.packageJson = JSON.parse(packageJson);
+
+   config.app = {
+      name: config.packageJson.name,
+      version: config.packageJson.version,
+   };
+
+   return config;
+};
+
